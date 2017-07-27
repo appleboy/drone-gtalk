0de142b6899fe57b9ab264b767ab72c97478de23
Bo-Yi Wu <appleboy.tw@gmail.com>
2017-07-27 09:54:09 +0800

    add new env
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func main() {
 		cli.StringSliceFlag{
 			Name:   "to",
 			Usage:  "send message to user",
-			EnvVar: "PLUGIN_TO",
+			EnvVar: "PLUGIN_TO,GOOGLE_TO",
 		},
 		cli.StringSliceFlag{
 			Name:   "message",
